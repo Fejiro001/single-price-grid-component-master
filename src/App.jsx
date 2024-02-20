9c871cd94ea188a15399803c46a33d30a681c3af
Abere Fejiro <aberefejiro@gmail.com>
2024-02-20 22:49:49 +0100

    formatted the html file and did the base structure for my grid component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,58 @@
-import './App.css'
+import "./App.css";
 
+const dollar = String.fromCodePoint(0x00024);
 function App() {
   return (
     <>
-      Join our community
+      <main>
+        <section>
+          <h1>Join our community</h1>
 
-30-day, hassle-free money back guarantee
+          <h2>30-day, hassle-free money back guarantee</h2>
 
-Gain access to our full library of tutorials along with expert code reviews. 
-Perfect for any developers who are serious about honing their skills.
+          <p>
+            Gain access to our full library of tutorials along with expert code
+            reviews. Perfect for any developers who are serious about honing
+            their skills.
+          </p>
+        </section>
 
-Monthly Subscription
+        <section>
+          <h3>Monthly Subscription</h3>
 
-&dollar;29 per month
+          <p>{dollar}29 per month</p>
 
-Full access for less than &dollar;1 a day
+          <p>Full access for less than &dollar;1 a day</p>
 
-Sign Up
+          <button>Sign Up</button>
+        </section>
 
-Why Us
+        <section>
+          <h3>Why Us</h3>
 
-Tutorials by industry experts
-Peer &amp; expert code review
-Coding exercises
-Access to our GitHub repos
-Community forum
-Flashcard decks
-New videos every week
+          <ul>
+            <li>Tutorials by industry experts</li>
+            <li>Peer &amp; expert code review</li>
+            <li>Coding exercises</li>
+            <li>Access to our GitHub repos</li>
+            <li>Community forum</li>
+            <li>Flashcard decks</li>
+            <li>New videos every week</li>
+          </ul>
+        </section>
+      </main>
 
-<footer>
-  <p class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </p>
-</footer>
+      <footer>
+        <p className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
